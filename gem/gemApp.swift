9d1b97509027c8582140fcd38d2e825d47f9b371
Rahuletto <rahulmarban@gmail.com>
2024-06-03 09:50:38 +0530

    Initial Commit

---
+++ gem/gemApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemApp.swift
+//  gem
+//
+//  Created by Rahul Marban on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct gemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
